df5723a31864d1f6d0925d0c3f12ed112fabbd4f
Dan <doates625@gmail.com>
2020-01-19 23:15:49 -0500

    Refactored ProgDisp with Timer class

--- ProgDisp.m
+++ ProgDisp.m
@@ -1,56 +1,57 @@
 classdef ProgDisp < handle
     %ProgDisp Class for reporting progress of long algorithms
     %   
-    %   This class periodically prints the progress of an algorithm and the expected execution time
-    %   remaining at a configurable time interval.
+    %   This class periodically displays the progress of an algorithm and the
+    %   expected remaining time at a configurable interval
     %   
     %   Author: Dan Oates (WPI Class of 2020)
     
     properties (Access = protected)
-        d_print;    % Print duration [s]
-        t_init;     % Time of reset [s]
-        t_print;    % Time of last print [s]
+        disp_int;   % Display interval [s]
+        init_tmr;   % Init timer [timing.Timer]
+        disp_tmr;   % Display timer [timing.Timer]
     end
     
     methods (Access = public)
-        function obj = ProgDisp(d_print)
-            %obj = PROGDISP(interval)
+        function obj = ProgDisp(disp_int)
+            %obj = PROGDISP(disp_int)
             %   Construct and start tracker
             %   
             %   Inputs:
-            %   - d_print = Print interval [s]
-            obj.d_print = d_print;
-            obj.start();
+            %   - disp_int = Display interval [s]
+            import('timing.Timer');
+            obj.disp_int = disp_int;
+            obj.init_tmr = Timer();
+            obj.disp_tmr = Timer();
         end
         
         function start(obj)
             %START(obj) Start (or reset) progress tracking
-            obj.t_init = tic();
-            obj.t_print = 0;
+            obj.init_tmr.tic();
+            obj.disp_tmr.tic();
         end
         
-        function printed = update(obj, prog)
-            %printed = UPDATE(obj, prog)
+        function do_disp = update(obj, prog)
+            %do_disp = UPDATE(obj, prog)
             %   Update progress tracker
             %   
             %   Inputs:
             %   - prog = Progress ratio [0, 1]
             %   
             %   Outputs:
-            %   - printed = Flag indicating print [logical]
-            t_now  = toc(obj.t_init);
-            printed = t_now - obj.t_print > obj.d_print;
-            if printed
-                obj.t_print = t_now;
-                d_left = (1.0 - prog) * t_now / prog;
-                if d_left > 86400
-                    str = sprintf('%.1f day(s)', d_left / 86400);
-                elseif d_left > 3600
-                    str = sprintf('%.1f hour(s)', d_left / 3600);
-                elseif d_left > 60
-                    str = sprintf('%.1f min(s)', d_left / 60);
+            %   - do_disp = Display flag
+            do_disp = obj.disp_tmr.elapsed(obj.disp_int);
+            if do_disp
+                obj.disp_tmr.tic();
+                t_left = (1.0 - prog) * obj.init_tmr.toc() / prog;
+                if t_left > 86400
+                    str = sprintf('%.1f day(s)', t_left / 86400);
+                elseif t_left > 3600
+                    str = sprintf('%.1f hour(s)', t_left / 3600);
+                elseif t_left > 60
+                    str = sprintf('%.1f min(s)', t_left / 60);
                 else
-                    str = sprintf('%.1f sec(s)', d_left);
+                    str = sprintf('%.1f sec(s)', t_left);
                 end
                 prog = prog * 100.0;
                 fprintf('Progress; %.3f%%, Time left: %s\n', prog, str);
